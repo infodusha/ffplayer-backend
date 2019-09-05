0f607ea385da24afc2ee819e3270c8e9e42ee7e6
infodusha <infodusha@ya.ru>
2019-09-05 21:59:43 +0300

    Also provide email for token query

--- src/typeDefs/Query.js
+++ src/typeDefs/Query.js
@@ -5,7 +5,7 @@ export default apollo.gql`
     "Filtered trainers list"
     trainers(rank: Rank, streamer: Boolean, game: Game): [Trainer]
     "Token by code"
-    token(code: String!): String @auth(required: false)
+    token(email: String!, code: String!): String @auth(required: false)
     "News part"
     news(first: Int, length: Int!): [News]
   }
